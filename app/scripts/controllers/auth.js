6cb6d8be2636bbebd689be6a7522df7fcdee7b7d
Jan Milosh <janmilosh@gmail.com>
2014-06-06 07:44:23 -0400

    Adds error handling for login and registration

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -11,13 +11,25 @@ app.controller('AuthController', function($scope, $location, Auth) {
 
   $scope.login = function() {
     Auth.login($scope.user).then(function() {
-      $location.path('/')
-    })
+      $location.path('/');
+    }, function(error) {
+      $scope.error = error.toString();
+    });
+  };
+
+  $scope.login = function () {
+    Auth.login($scope.user).then(function () {
+      $location.path('/');
+    }, function (error) {
+      $scope.error = error.toString();
+    });
   };
 
   $scope.register = function() {
     Auth.register($scope.user).then(function(authUser) {
       $location.path('/');
+    }, function(error) {
+      $scope.error = error.toString();
     });
   };
 });
