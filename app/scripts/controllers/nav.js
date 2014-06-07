5c9c6dde1d7a3e5b1e22f755b90978c20d7fe8db
Jan Milosh <janmilosh@gmail.com>
2014-06-07 18:45:40 -0400

    Retrieves current user for header upon page load

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,6 +1,11 @@
 'use strict';
 
-app.controller('NavController', function($rootScope, $scope, $location, Auth) {
+app.controller('NavController', function($rootScope, $scope, $location, Auth, User) {
+
+  if (User.signedIn()) {  //This may not be needed later,
+    User.getCurrent();    //but for now brings in the user to the header,
+  }                       //even on page refresh (puts current user on $rootScope again)
+
 
   $scope.logout = function() {    
     Auth.logout();         
